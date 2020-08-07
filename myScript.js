fed7a67cd8ac45285c759c6e820717e083aafe45
AVA77777 <69071343+AVA77777@users.noreply.github.com>
2020-08-07 15:27:36 +0400

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,50 @@
+window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth'
+  });
+
+  window.scrollBy({ 
+    top: 100,
+    left: 0, 
+    behavior: 'smooth' 
+  });
+
+  document.querySelector('.hello').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+
+
+
+
+
+
+  $(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
+    
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+  
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  })
+
